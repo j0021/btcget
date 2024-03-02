b521acbd8769b1d0ec7377d06b234c89f4e6c481
Joel Torres <joel@torreshub.com>
2024-03-02 12:00:31 -0600

    Update setup.py

--- btcget.py
+++ btcget.py
@@ -23,7 +23,6 @@
 
 # For more information, please refer to <https://unlicense.org>
 
-__version__ = "0.0.2"
 
 import argparse
 import yaml
--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
-from btcget import __version__
+
+with open("README.md") as f:
+    doc = f.read()
 
 setup(
     name="btcget",
-    version=__version__,
+    version="0.0.2",
+    description="Simple REST API wrapper for fetching Bitcoin market price data via multiple backends.",
+    long_description=doc,
+    long_description_content_type="text/markdown",
+    author="Joel Torres",
+    author_email="joel@torreshub.com",
+    url="https://github.com/j0021/btcget",
+    license="The Unlicense",
+    platforms="any",
     py_modules=["btcget"],
     install_requires=[
         "PyYAML==6.0.1",
@@ -13,5 +23,14 @@ setup(
         "console_scripts":[
             "btcget=btcget:_main"
         ]
-    }
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP"
+    ]
 )
